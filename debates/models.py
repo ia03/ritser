e9e25b31df68fb7db1fcc8ebdd38f437f7a4dfb1
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-06 15:48:57 -0500

    replaced textfields with htmlfields
    made textfields have rich text instead

--- debates/models.py
+++ debates/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from tinymce.models import HTMLField
 from accounts.models import User
 
 # Create your models here.
@@ -7,11 +8,10 @@ from accounts.models import User
 class Topic(models.Model):
 	name = models.CharField(max_length=30)
 	private = models.BooleanField(default=False)
-	description = models.TextField(max_length=500000)
+	description = models.HTMLField(max_length=600000)
 	created_on = models.DateField(auto_now_add=True)
 	created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics_created')
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics_owned')
-	#moderators = ArrayField(models.ForeignKey(User, on_delete=models.CASCADE))
 	moderators = models.ManyToManyField(User)
 	def __str__(self):
 		return self.name
@@ -21,6 +21,7 @@ class Debate(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='debates_owned')
 	topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='debates_contained')
 	question = models.CharField(max_length=300)
+	description = models.HTMLField(max_length=200000)
 	def __str__(self):
 		return self.question
 
@@ -29,6 +30,6 @@ class Argument(models.Model):
 	topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='arguments_contained')
 	debate = models.ForeignKey(Debate, on_delete=models.CASCADE, related_name='arguments_contained')
 	title = models.CharField(max_length=300)
-	body = models.TextField(max_length=100000)
+	body = models.HTMLField(max_length=200000)
 	def __str__(self):
 		return self.title

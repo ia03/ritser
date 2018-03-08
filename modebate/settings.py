672e15cccca7573e03695c7d69c1f6d63fe003d5
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-08 17:10:06 -0500

    made database pass an environment variable

--- modebate/settings.py
+++ modebate/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'modebate',
 		'USER': 'postgres',
-		'PASSWORD': 'Amb9Y9sXHgG993NW',
+		'PASSWORD': os.environ['dbpass'],
 		'HOST': '127.0.0.1',
 		'PORT': '5432',
     }

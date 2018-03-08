c8b3676668baafb8c54ae7ba524b2c85f79b7de6
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-08 18:33:36 -0500

    added terms and privacy pages

--- debates/urls.py
+++ debates/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('privacy', views.privacy, name='privacy'),
+	path('terms', views.terms, name='terms'),
 	path('t/<slug:tname>', views.topic, name='topic'),
 ]

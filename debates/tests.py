8a090232a0d6c0495e9a06aba08ea7c2ec48f4b5
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-08 11:16:09 -0500

    fixed indentation

--- debates/tests.py
+++ debates/tests.py
@@ -11,21 +11,21 @@ from .views import index, topic
 
 class ViewTestCase (TestCase):
     user = User.objects.create_user(username="test", email="test@test.com", password="test123")
-  def setUp(self):
-    test_topic = Topic.objects.create(name="test", private=False, owner=user, moderators=[user], created_by=[user])
-    test_topic.save()
-  def test_indexpage(self):
-    found = resolve('/')
-    self.assertEqual(found.func, index)
-  def test_topicpage(self):
-    found = resolve('/t/afw')
-    self.assertEqual(found.func, topic)
-  def test_invalidtopic(self):
-    response = self.client.get('t/akvurov_u439j')
-    self.assertEqual(response.status_code, 404) #checks to see if an invalid topic url will return a 404 (NOT FOUND)
-  def test_validtopic(self):
-    response = self.client.get('t/test')
-    self.assertEqual(response.status_code, 200) #checks to see if a valid topic url will return a 200 (OK)
-  def test_topicnamepassed(self):
-    response = self.client.get('t/test')
-    self.assertEqual(response.context['topic'].name, 'test') #checks to see if a valid topic url will return a 200 (OK)
+    def setUp(self):
+        test_topic = Topic.objects.create(name="test", private=False, owner=user, moderators=[user], created_by=[user])
+        test_topic.save()
+    def test_indexpage(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+    def test_topicpage(self):
+        found = resolve('/t/afw')
+        self.assertEqual(found.func, topic)
+    def test_invalidtopic(self):
+        response = self.client.get('t/akvurov_u439j')
+        self.assertEqual(response.status_code, 404) #checks to see if an invalid topic url will return a 404 (NOT FOUND)
+    def test_validtopic(self):
+        response = self.client.get('t/test')
+        self.assertEqual(response.status_code, 200) #checks to see if a valid topic url will return a 200 (OK)
+    def test_topicnamepassed(self):
+        response = self.client.get('t/test')
+        self.assertEqual(response.context['topic'].name, 'test') #checks to see if a valid topic url will return a 200 (OK)

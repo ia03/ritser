02ae7d7fe0098cc0c4ffa8e670ca7bd62d804908
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-07 19:04:06 -0500

    added approvedstatus to debate and slvls to topic

--- debates/models.py
+++ debates/models.py
@@ -10,6 +10,8 @@ class Topic(models.Model):
 	private = models.BooleanField(default=False)
 	description = models.TextField(max_length=600000, default='The description has not been set yet.', blank=True)
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics_owned')
+	slvl = models.IntegerField(default='0')
+	defdebslvl = models.IntegerField(default='0')
 	moderators = models.ManyToManyField(User, related_name='moderator_of')
 	created_on = models.DateTimeField(default=timezone.now)
 	created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics_created')
@@ -21,8 +23,9 @@ class Topic(models.Model):
 class Debate(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='debates_owned')
 	topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='debates')
-	slvl = models.IntegerField(default="0")
-	karma = models.IntegerField(default="0")
+	slvl = models.IntegerField(default='0')
+	approvedstatus = models.IntegerField(default='0')
+	karma = models.IntegerField(default='0')
 	users_upvoting = models.ManyToManyField(User, related_name='debates_upvoted')
 	active = models.BooleanField(default=True)
 	question = models.CharField(max_length=300)
@@ -37,7 +40,7 @@ class Argument(models.Model):
 	topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='arguments')
 	debate = models.ForeignKey(Debate, on_delete=models.CASCADE, related_name='arguments')
 	approvedstatus = models.IntegerField(default=0)
-	order = models.IntegerField(default="0")
+	order = models.IntegerField(default='0')
 	active = models.BooleanField(default=True)
 	title = models.CharField(max_length=300)
 	body = models.TextField(max_length=200000)
@@ -53,7 +56,7 @@ class ArgumentRevision(models.Model):
 	title = models.CharField(max_length=300)
 	body = models.TextField(max_length=200000)
 	approvedstatus = models.IntegerField(default=0)
-	order = models.IntegerField(default="0")
+	order = models.IntegerField(default='0')
 	datetime = models.DateTimeField(default=timezone.now)
 	def __str__(self):
 		return self.datetime
@@ -62,7 +65,7 @@ class ArgumentRevision(models.Model):
 class DebateRevision(models.Model):
 	debate = models.ForeignKey(Debate, on_delete=models.CASCADE, related_name='revisions')
 	topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='debaterevisions')
-	slvl = models.IntegerField(default="0")
+	slvl = models.IntegerField(default='0')
 	question = models.CharField(max_length=300)
 	description = models.TextField(max_length=200000, blank=True)
 	datetime = models.DateTimeField(default=timezone.now)

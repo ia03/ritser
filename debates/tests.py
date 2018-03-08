1bc1e451b4e922f0711777044086889ab31c301c
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-08 17:19:28 -0500

    fixed many-to-one direct assignment issue and invalid urls

--- debates/tests.py
+++ debates/tests.py
@@ -14,7 +14,8 @@ class ViewTestCase (TestCase):
         tuser = get_user_model().objects.create_user(username="test", email="test@test.com", password="test123")
         tuser.save()
         print(isinstance(tuser, User))
-        test_topic = Topic.objects.create(name="test", private=False, owner=tuser, moderators=[tuser], created_by=tuser)
+        test_topic = Topic.objects.create(name="test", private=False, owner=tuser, created_by=tuser)
+        test_topic.moderators.set([tuser])
         test_topic.save()
 
     def test_indexpage(self):
@@ -24,11 +25,11 @@ class ViewTestCase (TestCase):
         found = resolve('/t/afw')
         self.assertEqual(found.func, topic)
     def test_invalidtopic(self):
-        response = self.client.get('t/akvurov_u439j')
+        response = self.client.get('/t/akvurov_u439j')
         self.assertEqual(response.status_code, 404) #checks to see if an invalid topic url will return a 404 (NOT FOUND)
     def test_validtopic(self):
-        response = self.client.get('t/test')
+        response = self.client.get('/t/test')
         self.assertEqual(response.status_code, 200) #checks to see if a valid topic url will return a 200 (OK)
     def test_topicnamepassed(self):
-        response = self.client.get('t/test')
+        response = self.client.get('/t/test')
         self.assertEqual(response.context['topic'].name, 'test') #checks to see if a valid topic url will return a 200 (OK)

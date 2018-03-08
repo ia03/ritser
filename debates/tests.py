10716ba2c82492a2ae61a29cba6463c497ef6402
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-08 08:51:25 -0500

    added hello world test

--- debates/tests.py
+++ debates/tests.py
@@ -3,3 +3,8 @@ from django.utils import timezone
 from .models import Debate
 
 # Create your tests here.
+
+
+class DebateTestCase (TestCase):
+  def badmaths(self):
+    self.assertEqual(1 + 1, 4)

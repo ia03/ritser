3581d5cfa1dce12da0a9b57f946e39bed96e92d8
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-09 07:41:32 -0500

    added topicinfo view, changed topic view, added about view

--- debates/urls.py
+++ debates/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('privacy', views.privacy, name='privacy'),
 	path('terms', views.terms, name='terms'),
+	path('about', views.about, name='about'),
 	path('t/<slug:tname>', views.topic, name='topic'),
+	path('t/<slug:tname>/info', views.topicinfo, name='topicinfo'),
 ]
--- debates/views.py
+++ debates/views.py
@@ -9,20 +9,44 @@ def index(request):
 	}
 	return render(request, 'debates/index.html', context)
 
-def topic(request, tname):
-
-	topic = get_object_or_404(Topic, name=tname)
-
-
-	debates = topic.debates.all()
+def about(request):
 	context = {
-		'topic': topic,
-		'debates': debates,
+		'nbar': 'about',
 	}
-	return render(request, 'debates/topic.html', context)
+	return render(request, 'debates/about.html', context)
 
 def privacy(request):
 	return render(request, 'debates/privacy.html')
 
 def terms(request):
 	return render(request, 'debates/terms.html')
+
+def topic(request, tname):
+
+	topic = get_object_or_404(Topic, name=tname)
+	fmods = topic.moderators.all()[:10] #first 10 mods
+	mods = topic.moderators.all()
+
+	debates = topic.debates.all()
+	context = {
+		'fmods' : fmods,
+		'mods': mods,
+		'topic': topic,
+		'ctopicn': topic.name.capitalize(),
+		'debates': debates,
+	}
+	return render(request, 'debates/topic.html', context)
+
+def topicinfo(request, tname):
+
+	topic = get_object_or_404(Topic, name=tname)
+	mods = topic.moderators.all()
+
+	debates = topic.debates.all()
+	context = {
+		'mods': mods,
+		'topic': topic,
+		'ctopicn': topic.name.capitalize(),
+		'debates': debates,
+	}
+	return render(request, 'debates/topicinfo.html', context)

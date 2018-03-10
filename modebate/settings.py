88d2205a732401dcdf4f4b660a8b7cb7929fe2b8
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-10 13:59:42 -0500

    disabled s3 serving when DEBUG=True

--- modebate/settings.py
+++ modebate/settings.py
@@ -124,22 +124,22 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_URL = '/static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = 'modebate'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'debates/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-STATIC_URL = '/static/'
+if not DEBUG:
+	AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+	AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+	AWS_STORAGE_BUCKET_NAME = 'modebate'
+	AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+	AWS_S3_OBJECT_PARAMETERS = {
+	    'CacheControl': 'max-age=86400',
+	}
+	AWS_LOCATION = 'static'
+	STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'debates/static'),
+#]
+#STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+	

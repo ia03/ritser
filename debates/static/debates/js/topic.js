ca49ac18ad7206a6830a68565dd2176817a80002
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-09 21:30:29 -0500

    added for changing the sorting algorithm

---
+++ debates/static/debates/js/topic.js
@@ -0,0 +1,29 @@
+function changesort() {
+    var val = document.getElementById("sorts").value;
+    Cookies.set("dsort", val, { expires: 30 });
+    var x = window.location.search;
+    var argindex = x.indexOf("&sort=");
+    if (argindex != -1)
+    {
+      x = x.substring(0, x.indexOf("&sort=") - 1);
+    }
+    window.location.search = x + "&sort=" + val;
+}
+// IF arg is passed, cookie is NOT changed but arg takes priority
+// IF drop down is changed, cookie is changed
+//
+
+window.onload = function () {
+  var x = window.location.search.lastIndexOf("sort=");
+  var b = window.location.search.indexOf("&", x);
+  var c = Cookies.get("dsort");
+  if (b == -1) {
+    b = window.location.search.length;
+  }
+  if(x != -1) {
+    document.getElementById("sorts").value = window.location.search.substring(x+5, b);
+  } else if (c != null) {
+    document.getElementById("sorts").value = c;
+  }
+
+};

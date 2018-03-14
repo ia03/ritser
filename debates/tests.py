2bd79ae1ee20a09b1e346caf0db95f74ffe3dafb
ia03 <37108859+ia03@users.noreply.github.com>
2018-03-14 12:28:54 -0400

    combined all topic tests; added debate form test

--- debates/tests.py
+++ debates/tests.py
@@ -1,22 +1,28 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.utils import timezone
-from .models import Topic
+from .models import Topic, Debate
 from accounts.models import User
 from django.contrib.auth import get_user_model
 from .views import index, topic
+from .forms import DebateForm
 
 # Create your tests here.
 
-#todo: test topic view with no debates
-
 class ViewTestCase (TestCase):
     def setUp(self):
-        tuser = get_user_model().objects.create_user(username="test", email="test@test.com", password="test123")
+        tmod = User.objects.create_user(username="mod", email="test@test.com", password="test123")
+        tmod.save()
+        tuser = User.objects.create_user(username="user", email="test@test.com", password="test123")
         tuser.save()
-        test_topic = Topic.objects.create(name="test", private=False, owner=tuser, created_by=tuser)
-        test_topic.moderators.set([tuser])
+        tuser2 = User.objects.create_user(username="user2", email="test2@test.com", password="test123")
+        tuser2.save()
+        tgmod = User.objects.create_user(username="gmod", email="gmod@test.com", password="test123", modstatus=1)
+        test_topic = Topic.objects.create(name="test", private=False, owner=tmod, created_by=tmod)
+        test_topic.moderators.set([tmod])
         test_topic.save()
+        test_debate = Debate.objects.create(owner=tuser, topic=test_topic, question="Test debate")
+        test_debate.save()
 
     def test_indexpage(self):
         found = resolve('/')
@@ -24,14 +30,20 @@ class ViewTestCase (TestCase):
     def test_topicpage(self):
         found = resolve('/t/afw/')
         self.assertEqual(found.func, topic)
-    def test_invalidtopic(self):
         response = self.client.get('/t/akvurov_u439j/')
         self.assertEqual(response.status_code, 404) #checks to see if an invalid topic url will return a 404 (NOT FOUND)
-    def test_validtopic(self):
         response = self.client.get('/t/test/')
         self.assertEqual(response.status_code, 200) #checks to see if a valid topic url will return a 200 (OK)
-    def test_topicnamepassed(self):
         response = self.client.get('/t/test/')
         self.assertEqual(response.context['topic'].name, 'test') #checks to see if a valid topic url will pass the topic name
+    def test_debateform(self):
+        form_data = {
+            'topic_name': test_topic.name,
+            'question': 'testquestion1',
+            'description': 'testdescription1',
+        }
+        form = DebateForm(data=form_data, user=tuser, edit=0)
+        self.assertTrue(form.is_valid())
+        
 
 #todo: test debates page with wrong topic arg in url and see if it returns a 404

69c64dfaf8917253206a48c001187088970a3fdd
ia03 <ibrahimamsar@gmail.com>
2018-04-08 16:22:25 +0000

    unapproved base

--- modebate/settings.py
+++ modebate/settings.py
@@ -153,19 +153,21 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 if not DEBUG:
-	AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-	AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-	AWS_STORAGE_BUCKET_NAME = 'modebate'
-	AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-	AWS_S3_OBJECT_PARAMETERS = {
-	    'CacheControl': 'max-age=86400',
-	}
-	AWS_LOCATION = 'static'
-	STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_DIRS = [
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_STORAGE_BUCKET_NAME = 'modebate'
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_LOCATION = 'static'
+    
+    STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'debates/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    ]
+    
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 
 #Google recaptcha settings

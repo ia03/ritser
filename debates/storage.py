627a10e97cba66425033b689fd8dc9de1f9a25fe
ia03 <ibrahimamsar@gmail.com>
2018-04-29 02:24:25 +0000

    added s3 boto storage copy

---
+++ debates/storage.py
@@ -0,0 +1,16 @@
+from django.core.files.storage import get_storage_class
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class CachedS3Boto3Storage(S3Boto3Storage):
+    """
+    S3 storage backend that saves the files locally, too.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.local_storage = get_storage_class(
+            "compressor.storage.CompressorFileStorage")()
+
+    def save(self, name, content):
+        self.local_storage._save(name, content)
+        super().save(name, self.local_storage._open(name))
+        return name

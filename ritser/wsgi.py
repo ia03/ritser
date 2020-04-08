d45b23089dd76b50d302c7f941f7f299901a020f
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2020-04-08 00:22:15 +0000

    fixed wsgi.py

--- ritser/wsgi.py
+++ ritser/wsgi.py
@@ -1,16 +1,35 @@
-"""
-WSGI config for ritser project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+KEYS_TO_LOAD = [
+    'POSTGRES_PASSWORD',
+    'SECRET_KEY',
+    'GR_DEBATEFORM',
+    'GR_ARGUMENTFORM',
+    'GR_TOPICFORM',
+    'GR_SIGNUPFORM',
+    'GR_ADDEMAILFORM',
+    'GR_REPORTFORM',
+    'RECAPTCHA_PUBLIC',
+    'RECAPTCHA_PRIVATE',
+    'AWS_ACCESS_KEY_ID',
+    'AWS_SECRET_ACCESS_KEY'
+]
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ritser.settings")
 
-application = get_wsgi_application()
+def loading_app(wsgi_environ, start_response):
+    global real_app
+    import os
+    for key in KEYS_TO_LOAD:
+        try:
+            os.environ[key] = wsgi_environ[key]
+        except KeyError:
+            # The WSGI environment doesn't have the key
+            pass
+    from django.core.wsgi import get_wsgi_application
+    real_app = get_wsgi_application()
+    return real_app(wsgi_environ, start_response)
+
+real_app = loading_app
+
+application = lambda env, start: real_app(env, start)

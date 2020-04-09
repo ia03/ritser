15b526a5d4e325a56fa2398a08f3bf7ae862b639
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2020-04-09 03:17:31 -0400

    fixed static sitemap

--- debates/sitemaps.py
+++ debates/sitemaps.py
@@ -9,7 +9,7 @@ class StaticViewSitemap(Sitemap):
     changefreq = 'daily'
 
     def items(self):
-        return ['index', 'about', 'rules', 'terms', 'privacy', 'cookies']
+        return ['index', 'about', 'rules', 'terms', 'privacy']
 
     def location(self, item):
         return reverse(item)
@@ -18,10 +18,10 @@ class StaticViewSitemap(Sitemap):
 class TopicSitemap(Sitemap):
     changefreq = "daily"
     priority = 0.9
-    
+
     def items(self):
         return Topic.objects.all()
-    
+
     def lastmod(self, obj):
         return obj.edited_on
 
@@ -29,10 +29,10 @@ class TopicSitemap(Sitemap):
 class DebateSitemap(Sitemap):
     changefreq = "hourly"
     priority = 0.8
-    
+
     def items(self):
         return Debate.objects.filter(~Q(approvalstatus=3))
-    
+
     def lastmod(self, obj):
         return obj.edited_on
 
@@ -40,9 +40,9 @@ class DebateSitemap(Sitemap):
 class ArgumentSitemap(Sitemap):
     changefreq = "hourly"
     priority = 0.7
-    
+
     def items(self):
         return Argument.objects.filter(~Q(approvalstatus=3))
-    
+
     def lastmod(self, obj):
         return obj.edited_on
